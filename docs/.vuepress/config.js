2a28480d940a12e69f79bcb759c2b5a5bc820644
zhangquanming <419654548@qq.com>
2021-09-22 22:21:11 +0800

    项目初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,10 @@
 module.exports = {
   base: '/',
   dest: 'dist',
-  title: 'Vue.js 技术揭秘',
+  title: '张全明的笔记',
+  themeConfig: {
+    repo: 'zhangquanming/docs',
+    docsDir: 'docs',
+    lastUpdated: '上次更新',
+  }
 }
