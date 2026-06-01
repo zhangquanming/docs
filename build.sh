799a503e6f71e13a75d6f326e2d219ce286664c3
zhangquanming <419654548@qq.com>
2026-06-01 16:23:42 +0800

    fix: 修改推送脚本命令

--- build.sh
+++ build.sh
@@ -5,10 +5,18 @@ npm run build
 
 cd dist
 
+# 如果已存在 .git 文件夹，先删除
+if [ -d ".git" ]; then
+  rm -rf .git
+fi
+
 git init
+git config user.name "zhangquanming"
+git config user.email "419654548@qq.com"
 git add -A
 git commit -m 'deploy'
 
+# 先添加远程仓库，再推送
 git push -f git@github.com:zhangquanming/zhangquanming.github.io.git master
 
 cd -
